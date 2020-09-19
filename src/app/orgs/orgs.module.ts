af542d0898dd3c0fb1262877757d17dbe0f8309d
Simon Haugen <smn.hgn@gmail.com>
2020-09-19 16:07:16 +0200

    added styling for orgs

--- src/app/orgs/orgs.module.ts
+++ src/app/orgs/orgs.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { OrgListComponent } from './org-list/org-list.component';
 import { OrgItemComponent } from './org-item/org-item.component';
+import { IonicModule } from '@ionic/angular';
 
 const routes: Routes = [{ path: '', component: OrgListComponent }];
 
 @NgModule({
   declarations: [OrgListComponent, OrgItemComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), IonicModule],
 })
 export class OrgsModule {}
